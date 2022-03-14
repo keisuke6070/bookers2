8676d53c35cc1a806064f40ddff12c7098434dec
EC2 Default User <ec2-user@ip-172-31-13-16.ec2.internal>
2022-03-14 16:56:06 +0000

    [add]bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books,dependent: :destroy
 
- has_one_attached :profile_image
+  has_one_attached :profile_image
 
   def get_profile_image(width,height)
     unless profile_image.attached?
@@ -15,4 +15,4 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit:[width,height]).processed
   end
 
- end
+end
